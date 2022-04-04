dbe59d8d80593a680e1c2541186a0fc5fcad1dd5
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-04-04 18:09:19 +0530

    Update test_GenerateBatteryParameters.cpp

--- test_GenerateBatteryParameters.cpp
+++ test_GenerateBatteryParameters.cpp
@@ -10,10 +10,10 @@ TEST_CASE("To test if 10 integers are generated from function GenerateLinearExpV
   float RandomData[10];
   float ExpectedData[10] = {2, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8}; // with slope = 0.2 and xIntercept = 2
   int NumOfValues = SIZE_OF_ARRAY(RandomData);
-  linerExpParameters LinearExpressionParam = {0.2, 2}; // Slope, xIntercept
+  linerExpParameters LinearExpressionParam = {0.2, 2.0}; // Slope, xIntercept
   
   //call the FUT
   GenerateLinearExpValues(RandomData, LinearExpressionParam, NumOfValues);
   
-  REQUIRE(memcmp(RandomData, ExpectedData) == 0);
+  REQUIRE(memcmp(RandomData, ExpectedData, sizeof(RandomData)) == 0);
 }
