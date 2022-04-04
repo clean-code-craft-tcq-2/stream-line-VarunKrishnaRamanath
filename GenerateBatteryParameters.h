e52eb4c560bda9f6b6f0a316e8ad53809226b3e3
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-04-04 17:42:48 +0530

    Update GenerateBatteryParameters.h

--- GenerateBatteryParameters.h
+++ GenerateBatteryParameters.h
@@ -1,1 +1,11 @@
+#include <math.h>
 
+#define ROUND_OFF_TO_2_DECIMAL(Num)   floorf(((Num)*100)/100)
+#define SIZE_OF_ARRAY(array)  (sizeof(array)/sizeof(array[0]))
+
+typedef struct{
+  int slope;
+  int xIntercept;
+}linerExpParameters;
+
+void GenerateLinearExpValues(float* RandomData, linerExpParameters LinearExpressionParam, int NumOfValues);
