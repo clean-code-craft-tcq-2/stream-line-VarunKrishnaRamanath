5d393913ebd4b24439696a6701b65fcc5e363fc2
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-04-04 17:47:49 +0530

    Generating random values based on linear expression

--- GenerateBatteryParameters.c
+++ GenerateBatteryParameters.c
@@ -1,1 +1,16 @@
 #include "GenerateBatteryParameters.h"
+
+void GenerateLinearExpValues(float* RandomData, linerExpParameters LinearExpressionParam, int NumOfValues)
+{
+  float y;
+  int LoopCount = 0;
+  int x = 0;
+  
+  for(LoopCount = 0; LoopCount < NumOfValues; LoopCount++)
+  {
+    x = LoopCount+1;
+    y = ((LinearExpressionParam.slope*x) + LinearExpressionParam.xIntercept); // y = mx+c
+    RandomData[LoopCount] = ROUND_OFF_TO_2_DECIMAL(y);
+  }
+  
+}
