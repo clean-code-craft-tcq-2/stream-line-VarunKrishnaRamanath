01e4573a04576c696b1e964e533a85bb46ed5219
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-04-05 13:21:36 +0530

    Update GenerateBatteryParameters.h

--- GenerateBatteryParameters.h
+++ GenerateBatteryParameters.h
@@ -1,6 +1,6 @@
 #include <math.h>
 
-#define ROUND_OFF_TO_2_DECIMAL(Num)   (((Num)*100)/100)
+#define ROUND_OFF_TO_2_DECIMAL(Num)   (floor((Num)*100)/100)
 #define SIZE_OF_ARRAY(array)  (sizeof(array)/sizeof(array[0]))
 
 typedef struct{
