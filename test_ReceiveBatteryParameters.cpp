37aecebbebf6063d245473743f2508e1adaede2f
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-20 23:48:05 +0530

    Create test_ReceiveBatteryParameters.cpp

---
+++ test_ReceiveBatteryParameters.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include <string.h>
+#include "ReceiverBatteryParameters.h"
+#include <stdio.h>
+
+TEST_CASE("To test if 4 values")
+{
+  
+}
