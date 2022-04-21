42ce5fb7b69f6310772dd0bd8befd9692459a927
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-21 21:15:19 +0530

    Update test_ReceiveBatteryParameters.cpp

--- test_ReceiveBatteryParameters.cpp
+++ test_ReceiveBatteryParameters.cpp
@@ -4,7 +4,14 @@
 #include <string.h>
 #include <stdio.h>
 
-TEST_CASE("To test if 4 values")
+TEST_CASE("Test for Receiever to read from console") 
 {
-  
+  float temp[50], chargeRate[50]  = {0};
+  receiveDataFromConsole(temp,chargeRate);                                                              
+  float expectedTempValue[50] = {28.0, 28.1, 28.2, 28.3};
+  float expectedChargeRate[50] = {0.0, 0.50, 1.0, 1.5};
+  for(int i =0; i< 4; i++){
+  REQUIRE( (temp[i] - expectedTempValue[i]) <= 0.001);
+  REQUIRE( (chargeRate[i] - expectedChargeRate[i]) <= 0.001);
+  }
 }
