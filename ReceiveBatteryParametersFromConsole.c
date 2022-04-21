b896a29c9d5d29d1d819da2790e602705b6b20a6
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-21 21:19:10 +0530

    Update ReceiveBatteryParametersFromConsole.c

--- ReceiveBatteryParametersFromConsole.c
+++ ReceiveBatteryParametersFromConsole.c
@@ -1,14 +1,26 @@
 #include <string.h>
 #include <stdio.h>
 
-void readDataFromConsole()
-{
-  
+void receiveDataFromConsole(float* temperature, float* chargeRate) {
+    char unusedData[300];
+    for(int j = 0; j < 50; j++){
+        if(scanf("%50s", unusedData) == EOF) 
+        {
+            break;
+        } 
+        scanf("%f", &temperature[j]);     
+        scanf("%50s", unusedData);
+        scanf("%50s", unusedData);
+        scanf("%50s", unusedData);
+        scanf("%f", &chargeRate[j]);
+        printf("%.2f \t %.2f\n",temperature[j],chargeRate[j]);
+    }
+   
 }
 
 int main()
 {
-  
-  readDataFromConsole();
+  float temperature[100],chargeRate[100] = {0};
+  readDataFromConsole(temperature,chargeRate);
   return 0; 
 }
